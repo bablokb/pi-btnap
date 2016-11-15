d9c5161b76328dd770f70391b7d312ebe6e1b76a
Bernhard Bablok <bablokb@gmx.de>
2016-11-15 20:03:20 +0100

    initial entry

---
+++ files/usr/local/sbin/btnap.service.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# --------------------------------------------------------------------------
+# Wrapper script for btnap.service
+#
+# This script is a slightly pimped up version of the script found in
+# the post by Mike Kazantsev (See CREDITS for details).
+#
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-btnap
+#
+# --------------------------------------------------------------------------
+
+source /etc/btnap.conf
+
+# --- create bridge   ------------------------------------------------------
+
+if [ -n "$(brctl show $BR_DEV 2>&1 >/dev/null)" ]; then
+   brctl addbr "$BR_DEV"
+   brctl setfd "$BR_DEV" 0
+   brctl stp "$BR_DEV" off
+   ip addr add "$BR_IP/24" dev "$BR_DEV"
+   [ -n "$ADD_IF" ] && brctl addif "$BR_DEV" "$ADD_IF"
+   ip link set "$BR_DEV" up
+fi
+
+# --- start service-script -------------------------------------------------
+
+if [ "$MODE" = "server" ]; then
+  exec /usr/local/sbin/btnap.service.py ${DEBUG:+--debug} server $BR_DEV
+else
+  exec /usr/local/sbin/btnap.service.py ${DEBUG:+--debug} client $REMOTE_DEV
+fi
