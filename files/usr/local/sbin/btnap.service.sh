e865c939e7f472815dab4599ea2a78d882434be6
Bernhard Bablok <bablokb@gmx.de>
2016-12-07 18:40:28 +0100

    only create bridge for mode=SERVER / add route to gateway

--- files/usr/local/sbin/btnap.service.sh
+++ files/usr/local/sbin/btnap.service.sh
@@ -17,13 +17,16 @@ source /etc/btnap.conf
 
 # --- create bridge   ------------------------------------------------------
 
-if [ -n "$(brctl show $BR_DEV 2>&1 >/dev/null)" ]; then
-   brctl addbr "$BR_DEV"
-   brctl setfd "$BR_DEV" 0
-   brctl stp "$BR_DEV" off
-   ip addr add "$BR_IP" dev "$BR_DEV"
-   [ -n "$ADD_IF" ] && brctl addif "$BR_DEV" "$ADD_IF"
-   ip link set "$BR_DEV" up
+if [ "$MODE" = "server" ]; then
+  if [ -n "$(brctl show $BR_DEV 2>&1 >/dev/null)" ]; then
+     brctl addbr "$BR_DEV"
+     brctl setfd "$BR_DEV" 0
+     brctl stp "$BR_DEV" off
+     ip addr add "$BR_IP" dev "$BR_DEV"
+     [ -n "$ADD_IF" ] && brctl addif "$BR_DEV" "$ADD_IF"
+     ip link set "$BR_DEV" up
+     [ -n "$BR_GW" ] && ip route add default via "$BR_GW" dev "$BR_DEV"
+  fi
 fi
 
 # --- start service-script -------------------------------------------------
